adfe6c080ae5a38c60f4b6530c4ce5a5f62ee715
Mohamed Najiub <mohamednajiub@gmail.com>
2023-02-07 18:21:04 +0200

    fix: Game screen in landscape mode

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, Alert, FlatList } from 'react-native';
+import { View, StyleSheet, Text, Alert, FlatList, useWindowDimensions } from 'react-native';
 import CustomButton from '../components/ui/CustomButton';
 import Title from '../components/ui/Title';
 import generateRandomBetween from '../utils/generateRandomBetween';
@@ -17,6 +17,8 @@ const GameScreen = ({ userNumber, onGameOver }) => {
     const initialGuess = generateRandomBetween(1, 100, userNumber)
     const [currentGuess, setCurrentGuess] = useState(initialGuess)
     const [guessRounds, setGuessRounds] = useState([initialGuess])
+
+    const { width, height } = useWindowDimensions()
 
     const nextGuessHandler = (direction) => {
 
@@ -64,28 +66,55 @@ const GameScreen = ({ userNumber, onGameOver }) => {
     }, [])
 
     const guessRoundsListLength = guessRounds.length
+
+    let content = (<>
+        <NumberContainer>{currentGuess}</NumberContainer>
+        <Card>
+            <InstructionText style={{ marginBottom: 12 }}>Higher or lower?</InstructionText>
+
+            <View style={styles.btnsWrapper}>
+                <View style={{ flex: 1 }}>
+                    <CustomButton onPress={() => nextGuessHandler('higher')}>
+                        <Ionicons name='md-add' size={24} />
+
+                    </CustomButton>
+                </View>
+                <View style={{ flex: 1 }}>
+
+                    <CustomButton onPress={() => nextGuessHandler('lower')}>
+                        <Ionicons name='md-remove' size={24} />
+                    </CustomButton>
+                </View>
+            </View>
+        </Card>
+    </>);
+
+    if (width > 500) {
+        content = (<>
+            {/* <InstructionText style={{ marginBottom: 12 }}>Higher or lower?</InstructionText> */}
+            <View style={styles.btnsWrapper}>
+                <View style={{ flex: 1 }}>
+                    <CustomButton onPress={() => nextGuessHandler('higher')}>
+                        <Ionicons name='md-add' size={24} />
+
+                    </CustomButton>
+                </View>
+                <NumberContainer>{currentGuess}</NumberContainer>
+                <View style={{ flex: 1 }}>
+
+                    <CustomButton onPress={() => nextGuessHandler('lower')}>
+                        <Ionicons name='md-remove' size={24} />
+                    </CustomButton>
+                </View>
+            </View>
+
+        </>)
+    }
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
-            <NumberContainer>{currentGuess}</NumberContainer>
-            <Card>
-                <InstructionText style={{ marginBottom: 12 }}>Enter a Number</InstructionText>
-
-                <View style={styles.btnsWrapper}>
-                    <View style={{ flex: 1 }}>
-                        <CustomButton onPress={() => nextGuessHandler('higher')}>
-                            <Ionicons name='md-add' size={24} />
-
-                        </CustomButton>
-                    </View>
-                    <View style={{ flex: 1 }}>
-
-                        <CustomButton onPress={() => nextGuessHandler('lower')}>
-                            <Ionicons name='md-remove' size={24} />
-                        </CustomButton>
-                    </View>
-                </View>
-            </Card>
+            {content}
             <View style={styles.listContainer}>
                 <FlatList
                     data={guessRounds}
