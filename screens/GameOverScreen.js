a3bf9b7c484e22a435b5bc6adc2583ec4c30747f
Mohamed Najiub <mohamednajiub@gmail.com>
2023-02-07 20:27:58 +0200

    style(GameOverScreen): Handle orientation

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,24 +1,44 @@
-import { View, Image, StyleSheet, Text, Dimensions } from "react-native";
+import { View, Image, StyleSheet, Text, Dimensions, useWindowDimensions, ScrollView } from "react-native";
 import Title from '../components/ui/Title'
 import CustomButton from '../components/ui/CustomButton'
 import Colors from '../constants/colors';
 
 const GameOverScreen = ({ roundsNumber, userNumber, onStartNewGame }) => {
 
-    return (
-        <View style={styles.rootContainer}>
-            <Title>Game Over!</Title>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={require('../assets/images/success.png')} />
-            </View>
+    const { width, height } = useWindowDimensions();
 
-            <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> round to guess the number <Text style={styles.highlight}>{userNumber}</Text>.</Text>
-            <CustomButton onPress={onStartNewGame} style={{ marginTop: 20 }}>Start New Game</CustomButton>
-        </View>
+    let imageSize = 300;
+
+    if (width < 380) {
+        imageSize = 150;
+    }
+
+    if (height < 400) {
+        imageSize = 80
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
+
+    return (
+        <ScrollView style={{ flex: 1 }}>
+            <View style={styles.rootContainer}>
+                <Title>Game Over!</Title>
+                <View style={[styles.imageContainer, imageStyle]}>
+                    <Image style={styles.image} source={require('../assets/images/success.png')} />
+                </View>
+
+                <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> round to guess the number <Text style={styles.highlight}>{userNumber}</Text>.</Text>
+                <CustomButton onPress={onStartNewGame} style={{ marginTop: 20 }}>Start New Game</CustomButton>
+            </View>
+        </ScrollView>
     )
 }
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     rootContainer: {
@@ -28,9 +48,9 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     imageContainer: {
-        width: deviceWidth < 380 ? 150 : 300,
-        height: deviceWidth < 380 ? 150 : 300,
-        borderRadius: deviceWidth < 380 ? 75 : 150,
+        // width: deviceWidth < 380 ? 150 : 300,
+        // height: deviceWidth < 380 ? 150 : 300,
+        // borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: Colors.primary800,
         overflow: "hidden",
